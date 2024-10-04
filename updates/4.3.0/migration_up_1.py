eec298098d7f0e51aa51a2e3df20a62f20c3b75d
getcompass-opensource <opensource@getcompass.ru>
2024-10-04 21:41:59 +0400

    Release v4.3.0

---
+++ updates/4.3.0/migration_up_1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+
+sys.dont_write_bytecode = True
+
+from pathlib import Path
+import re, socket, yaml, argparse, readline, string, random, pwd, os, shutil
+
+current_script_path = Path(__file__).parent
+utils_path = current_script_path.parent.parent / 'script'
+sys.path.append(str(utils_path))
+
+from utils import scriptutils
+
+scriptutils.assert_root()
+script_dir = str(Path(__file__).parent.resolve())
+
+# папка, где находятся конфиги
+config_path = Path(script_dir + "/../../configs/database.yaml")
+
+# папка, где находятся шаблоны конфигов
+config_tpl_path = Path(script_dir + "/../../yaml_template/configs/database.tpl.yaml")
+
+# если конфиг существует, прекращаем выполнение
+if config_path.exists():
+    exit(0)
+
+# копируем конфиг
+shutil.copy2(str(config_tpl_path.resolve()), str(config_path.resolve()))
